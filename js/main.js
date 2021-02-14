88948715ed60fd0592b35b866e2bf9879b0febea
Alejandro Padilla <alejandropg339@gmail.com>
2021-02-13 22:19:39 -0500

    Añado portafolio al 80% buscar fuentes y terminar responsive agregar efectos con gsap y tecnologias iconos

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const typed = new Typed('.typed', {
+    strings: [
+        '<i class="writing">Desarollador Web</i>',
+        '<i class="writing">Estudiante de ingeniería de sistemas</i>',
+        '<i class="writing">Desarrollador Fullstack</i>'],
+    typeSpeed: 75, //volicidad en milisegundos para poner las letras
+    startDealy: 300, //tiempo de retraso en iniciar la animacíon, aplica para cuando termina y vuelve a iniciar
+    backSpeed: 65, // velocidad en milisegundos al borrar una letra
+    backDelay: 900, // tiempo que la aplicación espera para que leas la palabra antes de borrarla
+    loop: true, //reinicia el ciclo luego de terminarlo
+    loopCount: false, // al ponerlo en false le estamos diciendo que se repita una cantidad onfinita de veces 
+    contentType: 'html' // Es el formato en el que aparecera lo que se dijite si se pone null mostrara las etiquetas que se coloquen en htmls
+});
+
+gsap.to('#ilustration', { 
+     duration: 2,
+     ease: "sine.out",
+     y: -20,
+     repeat:-1,
+     yoyo:true,
+     }
+     );
